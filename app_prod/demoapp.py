1072551bb78fffb09c0eaf7721a6fe9f9b359c02
digvjiay-plivo <46581274+digvjiay-plivo@users.noreply.github.com>
2019-03-01 06:42:17 +0530

    First upload

---
+++ app_prod/demoapp.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, request, make_response, Response, Flask, flash, redirect, render_template, request, session, abort
+from flask_assets import Bundle, Environment
+
+import plivo
+from plivo import plivoxml
+
+app = Flask(__name__, static_url_path='')
+
+js = Bundle('', output = 'gen/main.js')
+css = Bundle('bootstrap.css','chosen.css','style.css','Vbootstrap.css','Vchosen.css','Vstyle.css',output = 'gen/main.css')
+
+assets = Environment(app)
+
+assets.register('main_js','js')
+assets.register('main_css','css')
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/send_sms/', methods=['GET', 'POST'])
+def outbound_sms():
+    
+    auth_id = 'MAMGI5OTMXZTI0MMVMOG'
+    auth_token = 'ZThmMzRjN2I1Y2U0ZWY2MDRkOTUxNTMzZjYzMzlh'
+    from_number= request.form.get("src_num")
+    to_number= request.form.get("dst_num")
+    content= request.form.get("text")
+    
+    '''
+        from_number = '1234567'
+        to_number = '919106054324'
+        content = 'test' '''
+    
+    #print from_number, to_number, content
+    # import pdb;pdb.set_trace()
+    
+    if not content:
+        content = "Test SMS with Plivo!"
+
+    client = plivo.RestClient(auth_id, auth_token)
+    try:
+        response = client.messages.create(
+                                          src= from_number,
+                                          dst= to_number,
+                                          text= content,
+                                          )
+        return render_template('result.html')
+        print(response.__dict__)
+        return response
+
+    except plivo.exceptions.PlivoRestError as e:
+        print(e)
+#response = p.send_message(params)
+# Prints the complete response
+
+@app.route('/send_message/', methods=['GET', 'POST'])
+def outbound_sms_template():
+    return render_template('test.html')
+
+#CALL STARTS HERE
+
+@app.route('/success/', methods=['GET', 'POST'])
+def call():
+    
+    auth_id = 'MAMGI5OTMXZTI0MMVMOG'
+    auth_token = 'ZThmMzRjN2I1Y2U0ZWY2MDRkOTUxNTMzZjYzMzlh'
+    
+    
+    from_number= request.form.get("src_num")
+    to_number= request.form.get("dst_num")
+    url= request.form.get("Answer_URL")
+    '''
+        
+        from_number = '1234567'
+        to_number = '919106054324'
+        url = 'http://s3.amazonaws.com/static.plivo.com/answer.xml'
+        '''
+    
+    # import pdb;pdb.set_trace()
+
+    if not url:
+       url = "http://s3.amazonaws.com/static.plivo.com/answer.xml"
+    
+    client = plivo.RestClient(auth_id, auth_token)
+    try:
+        call_made = client.calls.create(
+                                        from_= from_number,
+                                        to_= to_number,
+                                        answer_url= url,
+                                        answer_method = 'GET',
+                                        )
+        return render_template('result_call.html')
+        print(response.__dict__)
+        return response
+
+    except plivo.exceptions.PlivoRestError as e:
+        print(e)
+#response = p.send_message(params)
+# Prints the complete response
+
+@app.route('/call/', methods=['GET', 'POST'])
+
+def outbound_call_template():
+    return render_template('demoVoice.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
